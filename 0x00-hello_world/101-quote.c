d7f651414805ca1a00d84a6123017b78a0bf2f1c
abdulrhman <ewwe1198@gmail.com>
2023-07-13 07:31:00 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point
+ *
  * Description: print a qoute using write function
  *	ssize_t write(int fd.const void *buf,size_t count)
  *
  * Return: 1 (not success)
 */
+
+
 int main(void)
 {
 	char quo[] = "and that piece of art is uiseful\" - Dora Korpar, 2015-10-19\n";
